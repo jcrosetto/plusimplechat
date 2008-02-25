714bdaf5a142f0f8d70c499e2f20dc51638c9ba1
dex1304 <dex1304@6d61d4a5-ff46-0410-9073-f3f9481b7c33>
2008-02-25 19:03:00 +0000

    Changed port

--- trunk/cs320/simplechat1/EchoServer.java
+++ trunk/cs320/simplechat1/EchoServer.java
@@ -22,7 +22,7 @@ public class EchoServer extends AbstractServer
   /**
    * The default port to listen on.
    */
-  final public static int DEFAULT_PORT = 5556;
+  final public static int DEFAULT_PORT = 5555;
   
   //Constructors ****************************************************
   
