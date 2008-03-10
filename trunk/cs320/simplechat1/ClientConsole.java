2a41d715187e705d2204c6b82d7a6c9965cce0c9
thelatterrain <thelatterrain@6d61d4a5-ff46-0410-9073-f3f9481b7c33>
2008-03-10 04:22:42 +0000

    51 a done, only checks for r loginId

--- trunk/cs320/simplechat1/ClientConsole.java
+++ trunk/cs320/simplechat1/ClientConsole.java
@@ -41,11 +41,11 @@ public class ClientConsole implements ChatIF
    * @param host The host to connect to.
    * @param port The port to connect on.
    */
-  public ClientConsole(String host, int port) 
+  public ClientConsole(String loginID, String host, int port) 
   {
     try 
     {
-      client= new ChatClient(host, port, this);
+      client= new ChatClient(loginID, host, port, this);
     } 
     catch(IOException exception) 
     {
@@ -104,20 +104,26 @@ public class ClientConsole implements ChatIF
    */
   public static void main(String[] args) 
   {
+    String loginID = "";
     String host = "";
     int port = 0;  //The port number
 
+    if(args.length < 1){
+	System.out.println("You must specify a login ID, duchebag!");
+	System.exit(0);
+    }
+    
     try
     {
-      host = args[0];
-      port = Integer.parseInt(args[1]);
+      host = args[1];
+      port = Integer.parseInt(args[2]);
     }
     catch(ArrayIndexOutOfBoundsException e)
     {
       host = "localhost";
       port = DEFAULT_PORT;
     }
-    ClientConsole chat= new ClientConsole(host, port);
+    ClientConsole chat= new ClientConsole(loginID, host, port);
     chat.accept();  //Wait for console data
   }
 }
