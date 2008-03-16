c6cfdc86f69323ca33340c733eb1afed488db5dc
dex1304 <dex1304@6d61d4a5-ff46-0410-9073-f3f9481b7c33>
2008-03-16 21:02:43 +0000

    Almost done

--- cs320/simplechat1/client/ChatClient.java
+++ cs320/simplechat1/client/ChatClient.java
@@ -1,6 +1,6 @@
 // This file contains material supporting section 3.7 of the textbook:
 // "Object Oriented Software Engineering" and is issued under the open-source
-// license found at www.lloseng.com 
+// license found at www.lloseng.com
 
 package client;
 
@@ -12,25 +12,21 @@ import java.io.*;
 * This class overrides some of the methods defined in the abstract
 * superclass in order to give more functionality to the client.
 *
-* @author Dr Timothy C. Lethbridge
-* @author Dr Robert Lagani&egrave;
-* @author Fran&ccedil;ois B&eacute;langer
-* @version July 2000
+* @version March 2008
 */
 public class ChatClient extends AbstractClient
 {
 	//Instance variables **********************************************
 	
 	/**
-	* The interface type variable.  It allows the implementation of 
+	* The interface type variable.  It allows the implementation of
 	* the display method in the client.
 	*/
-	private ChatIF clientUI; 
-	
+	private ChatIF clientUI;
 	/**
-	*The login ID of the client
+	* the loginID of the client
 	*/
-	private String loginID;
+	String loginID;
 	
 	
 	//Constructors ****************************************************
@@ -38,22 +34,20 @@ public class ChatClient extends AbstractClient
 	/**
 	* Constructs an instance of the chat client.
 	*
+	* @param loginID The clients loginID
 	* @param host The server to connect to.
 	* @param port The port number to connect on.
 	* @param clientUI The interface type variable.
 	*/
-	////////////////////////////////
-	////////////////////////////////
-	//////loginid
-	public ChatClient(String loginID, String host, int port, ChatIF clientUI) 
-	throws IOException 
+	public ChatClient(String loginID, String host, int port, ChatIF clientUI)
+	throws IOException
 	{
 		super(host, port); //Call the superclass constructor
 		this.clientUI = clientUI;
-		openConnection();
-		//System.out.println("test " + getInetAddress());
-		sendToServer("#login " + loginID);
 		this.loginID = loginID;
+		openConnection();
+		//Immediately send the login information to the server
+		sendToServer("#login " + loginID);
 	}
 	
 	
@@ -64,22 +58,22 @@ public class ChatClient extends AbstractClient
 	*
 	* @param msg The message from the server.
 	*/
-	public void handleMessageFromServer(Object msg) 
+	public void handleMessageFromServer(Object msg)
 	{
 		clientUI.display(msg.toString());
 	}
 	
 	/**
-	* This method handles all data coming from the UI            
+	* This method handles all data coming from the UI
 	*
-	* @param message The message from the UI.    
+	* @param message The message from the UI.
 	*/
 	public void handleMessageFromClientUI(String message)
 	{
-		//if first char of message is # then call method
-		//else do this crap
+		//check the first character of the message for the # sign
 		if(message.charAt(0) == '#')
 			clientCommand(message);
+		//check if connected, if not do not send message to server
 		else if(isConnected()){
 			try
 			{
@@ -92,8 +86,9 @@ public class ChatClient extends AbstractClient
 				quit();
 			}
 		}
+		//if not connected display error message
 		else{
-			clientUI.display("Not connected to server");	
+			clientUI.display("Not connected to server");
 		}
 	}
 	
@@ -109,10 +104,18 @@ public class ChatClient extends AbstractClient
 		catch(IOException e) {}
 		System.exit(0);
 	}
+	/**
+	* This method processes the message when there is a # as the first
+	* character.
+	*
+	* @param command The command to process
+	*/
 	public void clientCommand(String command){
+		//the quit command
 		if(command.equalsIgnoreCase("#quit")){
 			quit();
 		}
+		//the logoff command
 		else if(command.equalsIgnoreCase("#logoff")){
 			try
 			{
@@ -120,7 +123,9 @@ public class ChatClient extends AbstractClient
 			}
 			catch(IOException e) {}
 		}
+		//the login command
 		else if(command.equalsIgnoreCase("#login")){
+			//cannot login if you are already logged on
 			if(isConnected()){
 				clientUI.display("Already logged in");
 			}
@@ -129,20 +134,26 @@ public class ChatClient extends AbstractClient
 				try
 				{
 					openConnection();
+					//resend the login information
 					sendToServer("#login " + loginID);
 				}
 				catch(IOException e) {
-					clientUI.display("Unable to establish "
-						+ "a connection on port " + getPort());
+					clientUI.display("Unable to establish a" +
+						" connection on port " + getPort());
 				}
 			}
 		}
+		//gethost command
 		else if(command.equalsIgnoreCase("#gethost"))
 			clientUI.display("" + getHost());
+		//getport command
 		else if(command.equalsIgnoreCase("#getport"))
 			clientUI.display("" + getPort());
-		
+		//sethost command
+		//note the space after sethost, this ensures that a host is
+		//specified
 		else if(command.startsWith("#sethost ")){
+			//cannot sethost if already logged on
 			if(isConnected()){
 				clientUI.display("You must be logged off to do that");
 			}
@@ -152,33 +163,50 @@ public class ChatClient extends AbstractClient
 				clientUI.display("Host set to " + tempHost);
 			}
 		}
+		//setport commmand note space after command
 		else if(command.startsWith("#setport ")){
+			//cannot set port if connected
 			if(isConnected()){
 				clientUI.display("You must be logged off to do that");
 			}
 			else{
-				//should check if port is a number
 				String tempPort = command.substring(9, command.length());
-				setPort(Integer.parseInt(tempPort));
-				clientUI.display("Port set to " + tempPort);
+				//Try/catch to ensure that only numbers are set as ports
+				try
+				{
+					setPort(Integer.parseInt(tempPort));
+					clientUI.display("Port set to " + tempPort);
+				}
+				catch(NumberFormatException e){
+					clientUI.display("Port must be a number");
+				}
 			}
 		}
+		//catch anything that starts with #, but isnt a command
 		else{
 			clientUI.display("Invalid command");
 		}
 		
 	}
-	
-	
-	
-	
+	/**
+	* This method is called after the connection has been closed.
+	*/
 	public void connectionClosed() {
 		clientUI.display("Connection to " + getHost() + " closed.");
 	}
+	/**
+	* This method is called each time an exception is thrown by the client's
+	* thread that is waiting for messages from the server.
+	*
+	* @param exception the exception raised
+	*/
 	public void connectionException(Exception exception) {
 		clientUI.display("Connection to " + getHost() + " lost.");
 		quit();
 	}
+	/**
+	* This method is called after a connection has been established.
+	*/
 	public void connectionEstablished() {
 		clientUI.display("Welcome! You are connected to "+ getHost());
 	}
