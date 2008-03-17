a8091981a19e91eb61b8b588e8d38c3f2a4733d8
dreamtennis <dreamtennis@6d61d4a5-ff46-0410-9073-f3f9481b7c33>
2008-03-17 06:00:53 +0000

    client awaits input

--- cs320/simplechat1/ClientConsole.java
+++ cs320/simplechat1/ClientConsole.java
@@ -48,8 +48,7 @@ public class ClientConsole implements ChatIF
 		catch(IOException exception) 
 		{
 			System.out.println("Error: Can't setup connection!"
-				+ " Terminating client.");
-			System.exit(1);
+				+ " Awaiting command.");
 		}
 	}
 	
