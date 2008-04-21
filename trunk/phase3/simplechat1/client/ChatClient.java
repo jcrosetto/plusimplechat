72e42bf0dd3e6fbb5d5e23f25d302f7062ea846a
dex1304 <dex1304@6d61d4a5-ff46-0410-9073-f3f9481b7c33>
2008-04-21 06:35:58 +0000

    test

--- trunk/phase3/simplechat1/client/ChatClient.java
+++ trunk/phase3/simplechat1/client/ChatClient.java
@@ -264,9 +264,25 @@ public class ChatClient extends AbstractClient
 			}
 
 		}
-		//forward command
+		//block command
 		//first implementation on 4/19 by cory
 		else if (command.startsWith("#block ")){
+			if(!isConnected()){
+				clientUI.display("You must be logged on to do that");
+			}
+			else{
+				try{
+					sendToServer(command);
+				}
+				catch(IOException e){
+					clientUI.display("Unable to send message to server.");
+				}
+			}
+
+		}
+		//unblock command
+		//first implementation on 4/19 by cory
+		else if (command.startsWith("#unblock")){
 			if(!isConnected()){
 				clientUI.display("You must be logged on to do that");
 			}
